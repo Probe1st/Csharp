1c2f7411a2ce636d36639944511b700d3e053cf3
Probe1st <svintsitskiy.leha@mail.ru>
2023-03-13 18:54:55 +0500

    д

--- Module_5/Module_5_ex_5/Program.cs
+++ Module_5/Module_5_ex_5/Program.cs
@@ -15,7 +15,7 @@ namespace Module_5_ex_5
             if (n == 0) return m + 1;
             else if ((n > 0) && (m == 0)) return A(n - 1, 1);
             else if ((n > 0) && (m > 0)) return A(n - 1, A(n, m - 1));
-            else return 0;            
+            else return 0;
         }
         static void Main(string[] args)
         {
@@ -26,10 +26,10 @@ namespace Module_5_ex_5
             // Вычислить, используя рекурсию, функцию Аккермана:
             // A(2, 5), A(1, 2)
             // A(n, m) = m + 1, если n = 0,
-            //         = A(n - 1, 1), если n > 0, m = 0,
+            //         = A(n - 1, 1), если n <> 0, m = 0,
             //         = A(n - 1, A(n, m - 1)), если n > 0, m > 0.
             //
-            
+
             Console.WriteLine(A(3, 5));
             Console.WriteLine(A(1, 2));
 
